0fee18f4ddadd5860fca5a0b4840519347bc89eb
augedoo <augedoo@gmail.com>
2020-06-29 20:03:48 +0000

    Added responsiveness

---
+++ day5-app_list_modal/app.js
@@ -0,0 +1,19 @@
+// UI Var
+const UIshowModalBtn = document.querySelector('.show-modal-btn');
+const UIModal = document.querySelector('#modal');
+
+UIshowModalBtn.addEventListener('click', showModal);
+document.addEventListener('click', closeModal);
+
+function showModal(e) {
+  UIModal.classList.toggle('modal-open');
+  e.preventDefault();
+}
+
+function closeModal(e) {
+  if (!e.target.closest('#modal')) {
+    UIModal.classList.toggle('modal-close');
+  }
+
+  e.preventDefault();
+}
